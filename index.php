380eef72da4aab7baf600463029b0c759743eb1d
FRANCOZLP <francozpf1@gmail.com>
2022-10-20 18:40:22 -0500

    Primer commit:Barra de navegación

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include("template/cabecera.php");?>
+
+        <div class="jumbotron text-center">
+        <h1 class="display-3">Bienvenido al sitio web de la Tienda</h1>
+        <p class="lead">Consulta la mercadería </p>
+        <hr class="my-2">
+
+        <img width="400" src="img/banner.jpg" class="img-thumbnail rounded mx-auto d-block"/>
+        <p>Más información</p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver el inventario de productos</a>
+        </p>
+    </div>
+
+        
+<?php include("template/pie.php");?>
+
---
+++ template/cabecera.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta chaset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0T7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-84-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/boostrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ60W/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+   
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">E-Tienda </a>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="productos.php">Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="nosotros.php">Nosotros</a>
+            </li>
+
+        </ul>
+    </nav>
+
+
+    <div class="container">
+    </br></br>
+        <div class="row">
+        
