250468c244131c4bd7f4a362214c7fabb59766cb
FRANCOZLP <francozpf1@gmail.com>
2022-10-23 19:28:38 -0500

    file structures between user n'admin,log interface

---
+++ admin/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Administrador</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+
+                    <div class="card">
+                        <div class="card-header">
+                            Header
+                        </div>
+                        <div class="card-body">
+                            <h4 class="card-title">Title</h4>
+                            <p class="card-text">Text</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            Footer
+                        </div>
+                    </div>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+  </body>
+</html>
